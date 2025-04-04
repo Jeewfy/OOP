aa004bc4bc59ead8549d22d2720b0d6e84a7d018
Jeewfy <mikekapikan@yandex.ru>
2025-04-04 21:44:43 +0300

    XD

---
+++ dz3/task1.py
@@ -0,0 +1,13 @@
+class Note:
+    def __init__(self, name, text,):
+        self.name = name
+        self.text = text
+        self.notes ={}
+    
+
+    def create_note(self):
+        name = input("Введите название вашей заметки: ")
+        text = ''
+
+        self.notes(name) = text
+        
